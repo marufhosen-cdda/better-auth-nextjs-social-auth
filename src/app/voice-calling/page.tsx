71335c3ce9b1f4aaae975de638a000c522cbfe47
marufhosen-cdda <maruf.cdda@gmail.com>
2025-08-25 16:37:38 +0600

    feat: call component

---
+++ src/app/voice-calling/page.tsx
@@ -0,0 +1,12 @@
+import CallInterface from '@/components/core/CallInterface'
+import React from 'react'
+
+const VoiceCalling = () => {
+    return (
+        <div>
+            <CallInterface />
+        </div>
+    )
+}
+
+export default VoiceCalling
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -9,7 +9,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
     globalForPrisma.prisma ??
     new PrismaClient({
-        datasourceUrl: process.env.DATABASE_URL,
+        datasourceUrl: process.env.DATABASE_URL || "prisma+postgres://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqd3RfaWQiOjEsInNlY3VyZV9rZXkiOiJza19aS3RyZG5jREpDU2FhdW1IeU90bUciLCJhcGlfa2V5IjoiMDFLM0RLSFc3UURZQVRSRU1QQjBCTVIwMzYiLCJ0ZW5hbnRfaWQiOiIxNWQ1OTA0Zjc2OTBjZTE3YmQzNGZlYWI1YmUyZDJjM2JiYzNlMjE3NzA5NDcwMGE0ZmI2MjY1Nzc1ZmIwOTBkIiwiaW50ZXJuYWxfc2VjcmV0IjoiZGYyNTg5YzYtOGRmNi00ZDY1LWFjZGUtMWFhMDZhMjYwNzM2In0.ZBf38zahaApBtzJLUBMUV8KmjPmOkJ46hgAwyk_hbZA",
     }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
